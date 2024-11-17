a8b9ad4562f52ad58b1b2deb39a0e9af6368360b
Viserys89 <fistvicky@gmail.com>
2024-11-17 13:30:11 +0700

    mengubah output

--- app/cli/chat/reply.py
+++ app/cli/chat/reply.py
@@ -1,17 +1,17 @@
 
 from chat.message import message
 
-def tambah_komentar (komen):
-    if komen:
-        message.append(komen)
+def tambah_balasan (balas):
+    if balas:
+        message.append(balas)
         print ("anda telah menambahkan komentar")
     else:
         print ("Tidak ada komentar")
 
-def display_komentar ():
+def display_balasan ():
     if message:
-        for i, komen in enumerate (message, 1):
-            print (f"{i}. {komen}")
+        for i, balas in enumerate (message, 1):
+            print (f"{i}. {balas}")
     else:
         print ("Kosong")
 
@@ -23,17 +23,17 @@ while True:
     print("   |                     |")
     print("   └1─2─3────────────────┘")
 
-    print("\n1. Tambahkan komentar")
-    print("2. Lihat komentar")
+    print("\n1. Reply postingan")
+    print("2. Lihat reply")
     print("3. Keluar")
 
     memilih = input("Masukkan pilihan: ")
 
     if memilih == '1':
-        user_comment = input("Masukkan komentar Anda: ")
-        tambah_komentar(user_comment)
+        user_comment = input("Masukkan reply Anda: ")
+        tambah_balasan(user_comment)
     elif memilih == '2':
-        display_komentar()
+        display_balasan()
     elif memilih == '3':
         break
     else:
